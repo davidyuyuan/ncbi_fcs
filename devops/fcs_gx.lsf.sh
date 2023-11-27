7796bb6868d0fb91d9e5ada14a360a0e2f4ac605
davidyuyuan <davidyuan@ebi.ac.uk>
2023-11-27 16:25:39 +0000

    Created scripts to run fcsgx.

--- devops/fcs_gx.lsf.sh
+++ devops/fcs_gx.lsf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# https://github.com/ncbi/fcs/wiki/FCS-adaptor
+# https://github.com/ncbi/fcs/wiki/FCS-GX
 
 # DIR where the current script resides
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -18,6 +18,18 @@ head -n 5 "${DIR}/fcsgx/gx_out/fcsgx_test.fa.6973.taxonomy.rpt"
 head "${DIR}/fcsgx/gx_out/fcsgx_test.fa.6973.fcs_gx_report.txt"
 
 # Verify functionality: 'gxdb' database
-bsub -n 2 -M 524288 -q bigmem python3 "${DIR}/fcsgx/fcs.py" screen genome --fasta "${DIR}/fcsgx/fcsgx_test.fa.gz" --out-dir "${DIR}/fcsgx/gx_out/" --gx-db "${LOCAL_DB}/gxdb" --tax-id 6973
+#bsub -n 2 -M 524288 -q bigmem python3 "${DIR}/fcsgx/fcs.py" screen genome --fasta "${DIR}/fcsgx/fcsgx_test.fa.gz" --out-dir "${DIR}/fcsgx/gx_out/" --gx-db "${LOCAL_DB}/gxdb" --tax-id 6973
 #head -n 5 "${DIR}/fcsgx/gx_out/fcsgx_test.fa.6973.taxonomy.rpt"
 #head "${DIR}/fcsgx/gx_out/fcsgx_test.fa.6973.fcs_gx_report.txt"
+
+# TODO combine the last two in the same job
+#bsub -n 2 -M 524288 -q bigmem zcat "${DIR}/fcsgx/fcsgx_test.fa.gz" | python3 "${DIR}/fcsgx/fcs.py" clean genome --action-report "${DIR}/fcsgx/gx_out/fcsgx_test.fa.6973.fcs_gx_report.txt" --output "${DIR}/fcsgx/gx_out/clean.fasta" --contam-fasta-out "${DIR}/fcsgx/gx_out/contam.fasta"
+
+function run_fcs_py {
+  python3 "${DIR}/fcsgx/fcs.py" screen genome --fasta "${DIR}/fcsgx/fcsgx_test.fa.gz" --out-dir "${DIR}/fcsgx/gx_out/" --gx-db "${LOCAL_DB}/gxdb" --tax-id 6973
+  head -n 5 "${DIR}/fcsgx/gx_out/fcsgx_test.fa.6973.taxonomy.rpt"
+  head "${DIR}/fcsgx/gx_out/fcsgx_test.fa.6973.fcs_gx_report.txt"
+  zcat "${DIR}/fcsgx/fcsgx_test.fa.gz" | python3 "${DIR}/fcsgx/fcs.py" clean genome --action-report "${DIR}/fcsgx/gx_out/fcsgx_test.fa.6973.fcs_gx_report.txt" --output "${DIR}/fcsgx/gx_out/clean.fasta" --contam-fasta-out "${DIR}/fcsgx/gx_out/contam.fasta"
+}
+
+bsub -n 2 -M 524288 -q bigmem run_fcs_py
